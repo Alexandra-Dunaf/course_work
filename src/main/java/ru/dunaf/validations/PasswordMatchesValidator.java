a6c90d538d26eb4a0d5bcb1975c2e281648e9008
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2022-10-26 18:59:28 +0400

    data validation

---
+++ src/main/java/ru/dunaf/validations/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package ru.dunaf.validations;
+
+import ru.dunaf.annotations.PasswordMatches;
+import ru.dunaf.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+        }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
