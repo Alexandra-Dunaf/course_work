9d940312bc0a05b49ae29bed74aa10c780458be8
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2022-11-02 21:18:54 +0400

    create ImageUploadService and CommentService

---
+++ src/main/java/ru/dunaf/dto/CommentDTO.java
@@ -0,0 +1,14 @@
+package ru.dunaf.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class CommentDTO {
+
+    private Long id;
+    private String username;
+    @NotEmpty
+    private String message;
+}
